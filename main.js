13f062389dfa00f303a1ab2692a961e120fa7b5f
Mauricio <mauricio.tp_@outlook.com>
2021-12-10 12:07:29 -0300

    media querys

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const shop = document.querySelector('a.shop');
 const shopWrapper = document.querySelector('div.shopwrapper');
+const mobileBtn = document.querySelector('.menu-mobile');
+const categories = document.querySelector('.categories');
 
 function handleClick(event) {
   event.preventDefault();
@@ -7,4 +9,11 @@ function handleClick(event) {
   shopWrapper.classList.toggle('active');
 }
 
+function handleMobile(event) {
+  event.preventDefault();
+  event.target.classList.toggle('active');
+  categories.classList.toggle('active');
+}
+
 shop.addEventListener('click', handleClick);
+mobileBtn.addEventListener('click', handleMobile);
